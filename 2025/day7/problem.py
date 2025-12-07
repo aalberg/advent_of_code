4a77186e670c8d4fd2578b28e0c725787d71a4b9
aalberg <aralberg@gmail.com>
2025-12-06 21:21:59 -0800

    Cleaned up day 7

--- 2025/day7/problem.py
+++ 2025/day7/problem.py
@@ -14,66 +14,35 @@ PROBLEM = 7
 IN_DIR = f"C:\\Users\\Alex\\Documents\\Projects\\advent_of_code\\2025\\day{PROBLEM}"
 IN_PATTERN = "test.txt" if TEST == 1 else "input.txt"
 IN_FILE = os.path.join(IN_DIR, IN_PATTERN)
-NUMS = "0123456789"
-DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
-grid = []
-e = []
-
-
-def nextline(f):
-  line = f.readline()
-  if not line:
-    return ""
-  line = line.rstrip()
-  return line
-
-
-def inbounds(x, y):
-  return 0 <= x < len(grid[0]) and 0 <= y < len(grid)
 
 
 def main():
+  grid = []
   with open(IN_FILE, 'r', encoding='utf-8') as f:
     for line in f:
       line = line.rstrip()
       grid.append(line)
-      e.append([0] * len(line))
-  for l in grid:
-    print(l)
-  active = set()
-  active.add(grid[0].find("S"))
-  print(active)
-  active2 = defaultdict(int)
-  active2[grid[0].find("S")] = 1
-  total1 = 0
-  for y, row in enumerate(grid):
-    print(y, row)
-    # for splititer in re.finditer('\^', row):
-    #   col = splititer.start()
-    #   if col in active:
-    #     total1 += 1
-    #     active.remove(col)
-    #     active.add(col - 1)
-    #     active.add(col + 1)
-    new_a = defaultdict(int)
-    for a, c in active2.items():
+
+  active = defaultdict(int)
+  active[grid[0].find("S")] = 1
+  total = 0
+  for row in grid:
+    new_active = defaultdict(int)
+    for a, count in active.items():
       split = False
-      for splititer in re.finditer('\^', row):
+      for splititer in re.finditer(r'\^', row):
         col = splititer.start()
         if a == col:
           split = True
-          new_a[col - 1] += c
-          new_a[col + 1] += c
+          total += 1
+          new_active[col - 1] += count
+          new_active[col + 1] += count
       if not split:
-        new_a[a] += c
-    active2 = new_a
-    #print(active2)
+        new_active[a] += count
+    active = new_active
 
-  print(sum(active2.values()))
-  #print(active)
-  # print("part 1:", total1)
-
+  print("part 1:", total)
+  print("part 2:", sum(active.values()))
 
 
 if __name__ == "__main__":
